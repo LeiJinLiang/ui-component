596556e504fb0b6ae9a4851bb83e63d15d38dc7d
466179464@qq.com <466179464@qq.com>
2017-07-16 22:26:49 +0800

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,7 +13,8 @@ class Home extends Component {
         return(
             <section>
                 <Nav data = {nav}/>
-                <div style={{ marginTop : '50px'}}>
+                <div style={{ marginTop : '50px', padding : '0 20px 0 20px'}}>
+                     CONTACT :  <a href="mailto:reg_taozi@163.com">reg_taozi@163.com</a>
                     {this.props.children}
                 </div>
             </section>
