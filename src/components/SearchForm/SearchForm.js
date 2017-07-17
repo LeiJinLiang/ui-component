3d57a205ddc0e5fd92f5be896470d30f12c959ca
466179464@qq.com <466179464@qq.com>
2017-07-17 22:59:46 +0800

    update

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Icon from '../../images/search.png'
 import styles from './search.css'
 
 
-const SearchForm = ({}) => (
-    <div className = { styles.contanier }>
-        <header className={styles.head}>
-            <span className = {styles.bg} style= {{ backgroundImage: 'url(' + Icon + ')', }}></span>
-            <div className={styles.form}>
-                <input type="text" placeholder="Search for everything"/>
+class SearchForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value : '',
+            arr : []
+        }
+    }
+
+    handleChange = (e) => {
+        let _arr = []
+        if(e.target.value ){
+            _arr.push({ value : e.target.value })
+        }
+        this.state.arr = _arr
+        this.setState({
+            value : e.target.value
+        })
+    }
+
+    render() {
+        const { value , arr } = this.state
+        return(
+            <div className = { styles.contanier }>
+                <header className={styles.head}>
+                    <span className = {styles.bg} style= {{ backgroundImage: 'url(' + Icon + ')', }}></span>
+                    <div className={styles.form}>
+                        <input type="text" value = { value } onChange = { e => this.handleChange(e)} placeholder="Search for everything"/>
+                    </div>
+                </header>
+                <ul className={ arr.length>0?styles.content:''}>
+                    {arr.map((item,index)=> (
+                        <li className={ styles.item } key = {index}>{item.value}</li>
+                    ))}
+                </ul>
             </div>
-        </header>
-        <ul className={ styles.content }>
-            <li className={ styles.item }>icons</li>
-            <li className={ styles.item }>icons</li>
-            <li className={ styles.item }>icons</li>
-        </ul>
-    </div>
-)
+        )
+    }
+}
 
 export default SearchForm
