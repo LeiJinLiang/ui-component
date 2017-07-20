5abe8be272ec1f8ec4da58ec16e4231ab2cd80be
jinlei <jinleilei@shhxzq.com>
2017-07-20 17:21:24 +0800

    update

--- src/pages/About/components/Resume.js
+++ src/pages/About/components/Resume.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import CSSModules from 'react-css-modules'
+import styles from '../styles/resume.css'
 
-const Resume = ({}) => (
-    <div>
-        Resume
-    </div>
-)
+class Resume extends Component {
+    constructor (props) {
+        super(props)
+        this.state  = {
+            show : true
+        }
+    }
 
-export default Resume
+    handleToogle = () => {
+        this.setState({
+            show : !this.state.show
+        })
+    }
+
+    render() {
+        const { show } = this.state
+        return (
+            <div styleName = "container">
+                <ul>
+                    <li>
+                        <p>July 2014</p>
+                        <div className= {styles.left} styleName = {show?'height2':'height1'}>
+                            <i onClick = {this.handleToogle}></i>
+                            {show?<i></i>:''}
+                        </div>
+                        <div styleName = 'right'>
+                            <h1>cloudcross</h1>
+                            autoprefixer adds vendor prefixes, using data from Can I Use.autoprefixer adds vendor prefixes, using data from Can I Use.
+                            autoprefixer adds vendor prefixes, using data from Can I Use.autoprefixer adds vendor prefixes, using data from Can I Use.
+                            autoprefixer adds vendor prefixes, using data from Can I Use.autoprefixer adds vendor prefixes, using data from Can I Use.
+                        </div>
+                        <p>March 2016</p>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default CSSModules(Resume,styles)
