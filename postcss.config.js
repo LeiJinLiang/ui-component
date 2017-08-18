8ff5180ba23d872c4a229cf802d397ae78bc66aa
jinlei <jinleilei@shhxzq.com>
2017-08-18 15:49:36 +0800

    postcss-cssnext 配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     plugins: [
-        require('autoprefixer'),
+        // require('autoprefixer'),
         require('precss'),
+        require('postcss-import')({
+
+        }),
+        require('postcss-cssnext')({
+            browsers: ['last 2 versions', '> 5%'],
+        }),
         require('postcss-assets')({
             basePath : './src',
             loadPaths: []
